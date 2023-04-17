101102f34acc7068d86a0a46cbe92b0191abb6ec
Emon <emon.mhk69@gmail.com>
2023-04-18 03:16:07 +0600

    right side data loaded into ui

--- src/Pages/Shared/leftNav/Leftnav.jsx
+++ src/Pages/Shared/leftNav/Leftnav.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
 
 const Leftnav = () => {
 
     const [catagories , setCatgories] = useState([]);
 
     useEffect(()=>{
-
         fetch("http://localhost:5000/catagories")
         .then(res=> res.json())
         .then(data=> setCatgories(data))
-        .catch(er=>{
-            console.log(er.message);
-        })
-
+        .catch(er=> console.log(er.message))
     },[])
 
     return (
         <div>
-            <h2>Left nav</h2>
+            <h4>All Catagories</h4>
+            <div className="ps-3">
+                {
+                    catagories.map(catagory=>
+                         <p key={catagory.id}> <Link className='text-black fw-semibold text-decoration-none'> {catagory.name} </Link> </p>)
+                }
+            </div>
         </div>
     );
 };
