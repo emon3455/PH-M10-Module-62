8e91b95352ed7dd485a19df6dfc782db8568c75a
Emon <emon.mhk69@gmail.com>
2023-04-18 03:03:05 +0600

    load data from back end data of right side set into state

--- src/Pages/Shared/leftNav/Leftnav.jsx
+++ src/Pages/Shared/leftNav/Leftnav.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Leftnav = () => {
+
+    const [catagories , setCatgories] = useState([]);
+
+    useEffect(()=>{
+
+        fetch("http://localhost:5000/catagories")
+        .then(res=> res.json())
+        .then(data=> setCatgories(data))
+        .catch(er=>{
+            console.log(er.message);
+        })
+
+    },[])
+
     return (
         <div>
             <h2>Left nav</h2>
