d5943c1a48ae0cc5817bf4e104e1e09ab1fa0036
Emon <emon.mhk69@gmail.com>
2023-04-21 01:28:00 +0600

    catagory wise data loading done

--- src/Pages/NewsCard/NewsCard.jsx
+++ src/Pages/NewsCard/NewsCard.jsx
@@ -1,7 +1,10 @@
+import moment from 'moment';
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { FaBookmark , FaShareAlt, FaStar , FaEye  , FaRegStar} from "react-icons/fa";
+import Rating from 'react-rating';
 
 const NewsCard = ({ news }) => {
 
@@ -10,12 +13,18 @@ const NewsCard = ({ news }) => {
 
     return (
         <Card className="mb-4">
-            <Card.Header>
-                <div className="">
-                    <Image className='h-25 w-25' variant="top" src={author.img && author?.img} roundedCircle/>
+            <Card.Header className='d-flex justify-content-between align-items-center'>
+                <div className="h-100 d-flex align-items-center gap-2">
+                    <div className="">
+                        <Image style={{height: "50px"}} variant="top" src={author.img && author?.img} roundedCircle/>
+                    </div>
+                    <div className="mb-0 mt-2">
+                        <h5>{author?.name ? author.name : "Anonymous"}</h5>
+                        <p>{author?.published_date ? moment(author?.published_date).format("yyyy-mm-d") : "No Publish Date"}</p>
+                    </div>
                 </div>
-                <div className="">
-
+                <div className="fs-5">
+                    <FaBookmark/> <FaShareAlt/>
                 </div>
             </Card.Header>
             <Card.Body>
@@ -26,7 +35,24 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
                 
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex justify-content-between">
+                <div className="">
+                    <span>
+                        <Rating
+                            placeholderRating={rating.number}
+                            readonly
+                            emptySymbol={<FaRegStar></FaRegStar>}
+                            placeholderSymbol={<FaStar className='text-warning'></FaStar>}
+                            fullSymbol={<FaStar></FaStar>}
+                        />
+                    </span>
+                    <span className='mx-2'>{rating?.number}</span>
+                </div>
+                <div className="">
+                    <span className='mx-1'><FaEye/></span>
+                    {total_view}
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
