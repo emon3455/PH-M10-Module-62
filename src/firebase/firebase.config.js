0b099a23f830fd00b50412b82b80cc5308a2359b
Emon <emon.mhk69@gmail.com>
2023-04-29 02:26:20 +0600

    firebase info saved

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkIi3twymZjNYWQ0XObb8lv7hmhk5boU8",
-  authDomain: "dragon-newspaper-auth.firebaseapp.com",
-  projectId: "dragon-newspaper-auth",
-  storageBucket: "dragon-newspaper-auth.appspot.com",
-  messagingSenderId: "1071695416356",
-  appId: "1:1071695416356:web:351f147713efdbaee8dcb1"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 
